f5ea36a8f09be6490c1a035f56af2d57e610ad9b
unknown <rinatino90@mail.ru>
2016-02-04 22:09:06 +0300

    db_insert fix

--- telegrambot.py
+++ telegrambot.py
@@ -153,11 +153,11 @@ def check_mail():
 def db_insert(from_id, name):
     conn = sqlite3.connect('telegrambot.db')
     log_event('Opened database successfully')
-    cursor = conn.execute("SELECT user_id FROM footballer WHERE user_id == 11111")
+    cursor = conn.execute("SELECT user_id FROM footballer WHERE user_id == 222222")
     u = cursor.fetchall()
     if u is not None: return True
     else:
-        values = {'user_id': 11111, 'first_name': 'name', 'second_name': None, 'username': None, 'visit': None,
+        values = {'user_id': 222222, 'first_name': 'name', 'second_name': None, 'username': None, 'visit': None,
               'resp_date': None}
         conn.execute(
             "INSERT INTO footballer (user_id, first_name, second_name, username, visit, resp_date) VALUES (:user_id, :first_name, :second_name, :username, :visit, :resp_date)",
