eb52aed0e30c6abdc5798949f97f2dc8291e8ae1
unknown <rinatino90@mail.ru>
2016-02-13 19:34:01 +0300

    db to postgres

--- db_create.py
+++ db_create.py
@@ -10,9 +10,9 @@ conn = psycopg2.connect(
 )
 cur = conn.cursor()
 cur.execute('CREATE TABLE footballer (user_id INTEGER PRIMARY KEY, first_name VARCHAR(30), second_name VARCHAR(30), username VARCHAR(30), visit BOOLEAN, resp_date DATE, resp_time TIME WITHOUT TIME ZONE)')
-cur.commit()
+conn.commit()
 cur.execute('INSERT INTO footballer (user_id, first_name, second_name, username) VALUES(83109589, "Rinat", NULL, "plohoidurdom", NULL, NULL, NULL)')
-cur.commit()
+conn.commit()
 #cur.execute('CREATE TABLE footballer_visit (id INTEGER PRIMARY KEY, user_id INTEGER, visit BOOLEAN, resp_date DATETIME)')
 #con.commit()
 #cur.execute('INSERT INTO footballer_visit (id, user_id, visit, resp_date) VALUES(1, 83109589, 1, "2016-03-28 15:05:59")')
@@ -21,4 +21,4 @@ print cur.lastrowid
 
 cur.execute('SELECT * FROM footballer')
 print cur.fetchall()
-cur.close()
+conn.close()
