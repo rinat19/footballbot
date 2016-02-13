4d8f6cc158c0024fb1c802ae22123a06fdc4250f
unknown <rinatino90@mail.ru>
2016-02-13 18:28:27 +0300

    db to postgres

--- db_create.py
+++ db_create.py
@@ -8,16 +8,17 @@ conn = psycopg2.connect(
     host='ec2-54-217-202-109.eu-west-1.compute.amazonaws.com',
     port='5432'
 )
-conn.execute('CREATE TABLE footballer (user_id INTEGER PRIMARY KEY, first_name VARCHAR(30), second_name VARCHAR(30), username VARCHAR(30, visit BOOLEAN, resp_date DATETIME))')
-conn.commit()
-conn.execute('INSERT INTO footballer (user_id, first_name, second_name, username) VALUES(83109589, "Rinat", NULL, "plohoidurdom", NULL, NULL)')
-conn.commit()
+cur = conn.cursor()
+cur.execute('CREATE TABLE footballer (user_id INTEGER PRIMARY KEY, first_name VARCHAR(30), second_name VARCHAR(30), username VARCHAR(30, visit BOOLEAN, resp_date DATETIME))')
+cur.commit()
+cur.execute('INSERT INTO footballer (user_id, first_name, second_name, username) VALUES(83109589, "Rinat", NULL, "plohoidurdom", NULL, NULL)')
+cur.commit()
 #cur.execute('CREATE TABLE footballer_visit (id INTEGER PRIMARY KEY, user_id INTEGER, visit BOOLEAN, resp_date DATETIME)')
 #con.commit()
 #cur.execute('INSERT INTO footballer_visit (id, user_id, visit, resp_date) VALUES(1, 83109589, 1, "2016-03-28 15:05:59")')
 #con.commit()
-print conn.lastrowid
+print cur.lastrowid
 
-conn.execute('SELECT * FROM footballer')
-print conn.fetchall()
-conn.close()
+cur.execute('SELECT * FROM footballer')
+print cur.fetchall()
+cur.close()
